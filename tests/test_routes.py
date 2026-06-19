491437c9f475503eb04c2a2e797a1dc51c54d368
capstone <195570009+Moon-Knight13@users.noreply.github.com>
2026-06-19 06:21:05 -0400

    Update account functions and tested

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,3 +138,22 @@ class TestAccountService(TestCase):
         """It should no Read and Account that is not found"""
         response = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        test_account = AccountFactory()
+        response = self.client.post(
+        BASE_URL,
+        json=test_account.serialize(),
+        content_type="application/json"
+    )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    # update the account
+
+        new_account = response.get_json()
+        new_account["name"] = "Something Known"
+        response = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_account = response.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
